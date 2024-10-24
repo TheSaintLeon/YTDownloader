8f459119658743bed4ef7414c71015b4ddb45178
Leon <victorleonek@gmail.com>
2024-10-24 09:39:59 +0200

    Fikk legge til tekstfelt som tar i mot url lenke variabler

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from pytube import YouTube
 app = ctk.CTk()
 app.geometry ("400x600")
 app.title ("YouTube MP4 Downloader")
-ctk.set_appearance_mode("light")
+ctk.set_appearance_mode("system")
 
+# Tekst i vinduet
+title = ctk.CTkLabel(app, text="Legg til en YouTube lenke")
+title.pack(padx=10, pady=10)
 
-
+#tekst felte for lenken til videon  
+url_var = tkinter.StringVar()
+link = ctk.CTkEntry(app, width=300, height=40, textvariable=url_var)
+link.pack(padx=10, pady=10)
 
 # Lar appen kjøre
 app.mainloop()
