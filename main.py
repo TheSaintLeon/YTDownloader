598c6cb4666d86e413b12c76dab3eafbda26acc1
Leon <victorleonek@gmail.com>
2024-10-24 09:01:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter
+import customtkinter as ctk 
+from pytube import YouTube
+
+
+# app vinduet
+app = ctk.CTk()
+app.geometry ("400x600")
+app.title ("YouTube MP4 Downloader")
+ctk.set_appearance_mode("light")
+
+
+
+
+
+
+
+
+# Lar appen kjøre
+app.mainloop()
