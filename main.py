0c0cc14cc1b772176c809dbccb1ef76cd74de6bc
CrimsonKilla <victorleonek@gmail.com>
2024-10-27 16:48:06 +0100

    Har oppdatert koden med til å faktisk fungere når man klikker last ned knappen. Måtte også bytte fra pytube biblioteket til ytdlp fordi pytube er ødelagt og blir ikke fikset.

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import tkinter
-import customtkinter as ctk 
-from pytube import YouTube
+import customtkinter as ctk
+from yt_dlp import YoutubeDL
 
+def startDownload():
+    try:
+        ytLink = link.get()
+        ydl_opts = {
+            'format': 'best', # velger beste kvalitet for videon
+            'outtmpl': '%(title)s.%(ext)s', #setter fil navnet og fil type.
+        }
+        with YoutubeDL(ydl_opts) as ydl: # starter nedlastningen med tidligere valg
+            ydl.download([ytLink]) # laster ned
+    except Exception as e: # gir feilmelding hvis noe går galt
+        print(f"YouTube link fungerer ikke: {e}")
+    print("Video lastet ned")
 
 # app vinduet
 app = ctk.CTk()
-app.geometry ("400x600")
-app.title ("YouTube MP4 Downloader")
+app.geometry("400x600")
+app.title("YouTube MP4 Downloader")
 ctk.set_appearance_mode("system")
 
 # Tekst i vinduet
 title = ctk.CTkLabel(app, text="Legg til en YouTube lenke")
 title.pack(padx=10, pady=10)
 
-#tekst felte for lenken til videon  
+# tekstfelt for lenken til videoen  
 url_var = tkinter.StringVar()
 link = ctk.CTkEntry(app, width=300, height=40, textvariable=url_var)
 link.pack(padx=10, pady=10)
+
+# Last ned-knappen
+download = ctk.CTkButton(app, text="Last ned", command=startDownload)
+download.pack(padx=10, pady=10)
 
 # Lar appen kjøre
 app.mainloop()
